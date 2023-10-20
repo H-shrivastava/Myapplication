c6c83c7de0f90f3a3edf1f073b6a4502383ac710
Harsh_Shrivastava <harsh.shrivastava@z1media.com>
2023-10-20 18:47:01 +0530

    project path changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,12 @@ dependencies {
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(project(mapOf("path" to ":module2")))
+    implementation(project(mapOf("path" to ":module1")))
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    implementation (project(":module1"))
-    implementation (project(":module2"))
 //    implementation 'com.github.H-shrivastava.Myapplication:module1:1.0.2'
+
 
 }
