6b8394f8accc80c4ae2ea4e3195a9a4f81fe65ae
Harsh_Shrivastava <harsh.shrivastava@z1media.com>
2023-10-26 13:42:57 +0530

    testing1.0.13

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.example.myapplication.sample.module1.module1String
-import com.example.myapplication.sample.module2.module2String
+//import com.example.myapplication.sample.module1.module1String
+//import com.example.myapplication.sample.module2.module2String
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(TextView(this).apply {
             text = """
                 Hey there! Let's do some math!
-                 $module1String
-                 $module2String
+//                 $module1String
+//                 $module2String
             """.trimIndent()
         })
     }
