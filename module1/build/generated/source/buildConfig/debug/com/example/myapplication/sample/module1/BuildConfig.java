01225a2f0c2072f838579d2b8ec21eae430b46b2
Harsh_Shrivastava <harsh.shrivastava@z1media.com>
2023-10-26 16:42:03 +0530

    change minsdk version 31 to 33 and add android.disableAutomaticComponentCreation=truein gradle.properties

---
+++ module1/build/generated/source/buildConfig/debug/com/example/myapplication/sample/module1/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.myapplication.sample.module1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.myapplication.sample.module1";
+  public static final String BUILD_TYPE = "debug";
+}
