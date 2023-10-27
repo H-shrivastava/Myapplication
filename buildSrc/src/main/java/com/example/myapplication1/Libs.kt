1eeaeb33b8e1ac2a4fdfdec90f17cc57f6b477c1
Harsh_Shrivastava <harsh.shrivastava@z1media.com>
2023-10-27 11:28:08 +0530

    add sourcejaar type in build.gradle of modules

--- buildSrc/src/main/java/com/example/myapplication1/Libs.kt
+++ buildSrc/src/main/java/com/example/myapplication1/Libs.kt
@@ -13,7 +13,7 @@ object Libs {
     const val ERROR_PRONE_ANNOTATIONS = "com.google.errorprone:error_prone_annotations:2.16"
 
     object PlayServices {
-        const val ADS = "com.google.android.gms:play-services-ads:22.4.0"
+        const val ADS = "com.google.android.gms:play-services-ads:22.1.0"
         const val ADS_IDENTIFIER = "com.google.android.gms:play-services-ads-identifier:18.0.1"
     }
 
@@ -37,8 +37,8 @@ object Libs {
         const val OK_HTTP = "com.squareup.okhttp3:okhttp:4.9.1"
     }
     object Coroutine {
-        const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1"
-        const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1"
+        const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
+        const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
     }
 
     object Applovin {
@@ -49,9 +49,9 @@ object Libs {
 
     object Test {
 
-        const val JUNIT = "junit:junit:4.13.2"
-        const val ANDROIDX_JUNIT= "androidx.test.ext:junit:1.1.5"
-        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.5.1"
+        const val JUNIT = "junit:junit:4+"
+        const val ANDROIDX_JUNIT= "androidx.test.ext:junit:1.1.2"
+        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.3.0"
 
     }
 
