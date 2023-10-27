4858473246ef839fc0fa090001e43c573d85d790
Harsh_Shrivastava <harsh.shrivastava@z1media.com>
2023-10-27 11:52:46 +0530

    added sourcejar in build.gradle

--- buildSrc/src/main/java/com/example/myapplication1/Libs.kt
+++ buildSrc/src/main/java/com/example/myapplication1/Libs.kt
@@ -3,8 +3,8 @@ package com.example.myapplication1
 object Libs {
 
     const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:1.7.10"
-    const val CORE_KTX = "androidx.core:core-ktx:1.3.1" // old 1.3.1
-    const val MATERIAL_DESIGN = "com.google.android.material:material:1.2.1" // old latest 1.2.1
+    const val CORE_KTX = "androidx.core:core-ktx:1.7.0" // old 1.3.1
+    const val MATERIAL_DESIGN = "com.google.android.material:material:1.9.0" // old latest 1.2.1
     const val MULTI_DEX = "androidx.multidex:multidex:2.0.1"
     const val GSON = "com.google.code.gson:gson:2.8.9"
 
@@ -13,12 +13,12 @@ object Libs {
     const val ERROR_PRONE_ANNOTATIONS = "com.google.errorprone:error_prone_annotations:2.16"
 
     object PlayServices {
-        const val ADS = "com.google.android.gms:play-services-ads:22.1.0"
+        const val ADS = "com.google.android.gms:play-services-ads:22.4.0"
         const val ADS_IDENTIFIER = "com.google.android.gms:play-services-ads-identifier:18.0.1"
     }
 
     object AndroidX {
-        const val APPCOMPAT = "androidx.appcompat:appcompat:1.2.0"
+        const val APPCOMPAT = "androidx.appcompat:appcompat:1.6.1"
         const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
         const val CARD_VIEW = "androidx.cardview:cardview:1.0.0"
         const val WEBKIT = "androidx.webkit:webkit:1.4.0"
@@ -37,8 +37,8 @@ object Libs {
         const val OK_HTTP = "com.squareup.okhttp3:okhttp:4.9.1"
     }
     object Coroutine {
-        const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
-        const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
+        const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1"
+        const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1"
     }
 
     object Applovin {
@@ -49,9 +49,9 @@ object Libs {
 
     object Test {
 
-        const val JUNIT = "junit:junit:4+"
-        const val ANDROIDX_JUNIT= "androidx.test.ext:junit:1.1.2"
-        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.3.0"
+        const val JUNIT = "junit:junit:4.13.2"
+        const val ANDROIDX_JUNIT= "androidx.test.ext:junit:1.1.5"
+        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.5.1"
 
     }
 
